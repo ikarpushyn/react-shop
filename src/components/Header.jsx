ad1f4d1e39dc319c0ffd9a89b75a3eea637ab423
ilija <komperdel@gmail.com>
2022-10-05 18:42:40 +0300

    gh-pages added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,16 @@ export const Header = () => {
 	return (
 		<nav className="#4a148c purple darken-4">
 			<div className="nav-wrapper">
-				<a href="#!" className="brand-logo">
+				<a href="/" className="brand-logo">
 					React Shop
 				</a>
 				<ul id="nav-mobile" className="right hide-on-med-and-down">
 					<li></li>
 					<li></li>
 					<li>
-						<a href="collapsible.html">repo</a>
+						<a href="https://ikarpushyn.github.io/react-shop/" target="_blank" rel="noreferrer">
+							Repo
+						</a>
 					</li>
 				</ul>
 			</div>
