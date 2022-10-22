81901902a37fdffe1925f51a1436fb22833cc3c9
ilija <komperdel@gmail.com>
2022-10-22 16:22:11 +0300

    fix link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export const Header = () => {
 	return (
 		<nav className="#4a148c purple darken-4">
 			<div className="nav-wrapper">
-				<a href="https://ikarpushyn.github.io/react-shop/" className="brand-logo">
+				<a href="https://ikarpushyn.github.io/React_food/" className="brand-logo">
 					React Shop
 				</a>
 				<ul id="nav-mobile" className="right hide-on-med-and-down">
